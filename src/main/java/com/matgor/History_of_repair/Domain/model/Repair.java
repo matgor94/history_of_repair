7cf404e849ba036a91d67967659591ee674c92f4
matgor94 <maly316@vp.pl>
2021-09-28 19:23:05 +0200

    Entities

---
+++ src/main/java/com/matgor/History_of_repair/Domain/model/Repair.java
@@ -0,0 +1,31 @@
+package com.matgor.History_of_repair.Domain.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "repair")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Repair {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank @NotNull
+    private String desctription;
+
+    @NotBlank @NotNull
+    private int partsCost;
+    
+    @NotBlank @NotNull
+    private int jobCost;
+}
---
+++ src/main/java/com/matgor/History_of_repair/Domain/model/Vehicle.java
@@ -0,0 +1,35 @@
+package com.matgor.History_of_repair.Domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "vehicles")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull @NotBlank
+    private String producent;
+
+    @NotNull @NotBlank
+    private String model;
+
+    @NotNull @NotBlank
+    private int yearOfProduction;
+
+    @NotNull @NotBlank
+    private int km;
+
+    @NotNull @NotBlank
+    private int capacity;
+}
