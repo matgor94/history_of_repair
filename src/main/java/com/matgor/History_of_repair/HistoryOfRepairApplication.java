6cdfff366fe32e2f8806ed06bcc74d3ac1e92aa4
matgor94 <maly316@vp.pl>
2021-09-28 18:25:14 +0200

    Create project - first commit

---
+++ src/main/java/com/matgor/History_of_repair/HistoryOfRepairApplication.java
@@ -0,0 +1,13 @@
+package com.matgor.History_of_repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryOfRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryOfRepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matgor/History_of_repair/HistoryOfRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matgor.History_of_repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryOfRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
